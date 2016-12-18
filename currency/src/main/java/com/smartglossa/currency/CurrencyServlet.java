aeb931519a26711ee9e3f69aeef0b28d1bf2a2d4
Harish <hariharan@smartglossa.com>
2016-12-18 17:12:36 +0530

    add some files

--- currency/src/main/java/com/smartglossa/currency/CurrencyServlet.java
+++ currency/src/main/java/com/smartglossa/currency/CurrencyServlet.java
@@ -14,46 +14,48 @@ import javax.servlet.http.HttpServletResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-
 public class CurrencyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    
-    public CurrencyServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	public CurrencyServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// response.getWriter().append("Served at:
+		// ").append(request.getContextPath());
 		doPost(request, response);
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-String operation = request.getParameter("operation");
-		
+		String operation = request.getParameter("operation");
+
 		if (operation.equals("add")) {
 			JSONObject obj = new JSONObject();
-			int rs1 = Integer.parseInt("rs1");
-			int rs2 = Integer.parseInt("rs2");
-			int rs5 = Integer.parseInt("rs5");
-			int rs10 = Integer.parseInt("rs10");
-			int rs20 =Integer.parseInt("rs20");
-			int rs50 = Integer.parseInt("rs50");
-			int rs100 = Integer.parseInt("rs100");
-			int rs500 = Integer.parseInt("rs500");
-			int rs1000 = Integer.parseInt("rs1000");
-			int rs2000 = Integer.parseInt("rs2000");
-			int total = Integer.parseInt("total");
+			int rs1 = Integer.parseInt(request.getParameter("rs1"));
+			int rs2 = Integer.parseInt(request.getParameter("rs2"));
+			int rs5 = Integer.parseInt(request.getParameter("rs5"));
+			int rs10 = Integer.parseInt(request.getParameter("rs10"));
+			int rs20 = Integer.parseInt(request.getParameter("rs20"));
+			int rs50 = Integer.parseInt(request.getParameter("rs50"));
+			int rs100 = Integer.parseInt(request.getParameter("rs100"));
+			int rs500 = Integer.parseInt(request.getParameter("rs500"));
+			int rs1000 = Integer.parseInt(request.getParameter("rs1000"));
+			int rs2000 = Integer.parseInt(request.getParameter("rs2000"));
+			int total = Integer.parseInt(request.getParameter("total"));
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
-				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Currency","root","root");
+				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/currency", "root",
+						"root");
 				Statement statement = connection.createStatement();
-				String query ="insert into (one,two,five,ten,twenty,fivty,oneh,fiveh,onet,twot,total)values("+rs1+","+rs2+","+rs5+","+rs10+","+rs20+","+rs50+","+rs100+","+rs500+","+rs1000+","+rs2000+","+total+")";
+				String query = "insert into calculation(one,two,five,ten,twenty,fivty,oneh,fiveh,onet,twot,total)values("
+						+ rs1 + "," + rs2 + "," + rs5 + "," + rs10 + "," + rs20 + "," + rs50 + "," + rs100 + "," + rs500
+						+ "," + rs1000 + "," + rs2000 + "," + total + ")";
 				statement.execute(query);
 				obj.put("status", "success");
 				response.getWriter().print(obj);
@@ -62,8 +64,7 @@ String operation = request.getParameter("operation");
 				e.printStackTrace();
 				obj.put("status", "fail");
 			}
-	}
-		
+		}
 
 	}
-	}
+}
--- currency/src/main/webapp/JS/currency.js
+++ currency/src/main/webapp/JS/currency.js
@@ -6,28 +6,40 @@ $(window).on(
       					'click',
       					'#butt',
       					function() {
-                  var one=$('#1').val();
-                  var onem=one*1;
-                  var two=$('#2').val();
-                  var twom=two*2;
-                  var five=$('#5').val();
-                  var fivem=five*5;
-                  var ten=$('#10').val();
-                  var tenm=ten*10;
-                  var twty=$('#20').val();
-                  var twtym=twty*20;
-                  var fif=$('#50').val();
-                  var fifm=fif*50;
-                  var hunt=$('#100').val();
-                  var huntm=hunt*100;
-                  var fhunt=$('#500').val();
-                  var fhuntm=fhunt*500;
-                  var thous=$('#1000').val();
-                  var thousm=thous*1000;
-                  var twthous=$('#2000').val();
-                  var twthousm=twthous*2000;
-                  var add=onem+twom+fivem+tenm+twtym+fifm+huntm+fhuntm+thousm+twthousm;
-                  $('#answer').val(add);
+                  var one=$('#rs1').val();
+                  var rs1=one*1;
+                  var two=$('#rs2').val();
+                  var rs2=two*2;
+                  var five=$('#rs5').val();
+                  var rs5=five*5;
+                  var ten=$('#rs10').val();
+                  var rs10=ten*10;
+                  var twty=$('#rs20').val();
+                  var rs20=twty*20;
+                  var fif=$('#rs50').val();
+                  var rs50=fif*50;
+                  var hunt=$('#rs100').val();
+                  var rs100=hunt*100;
+                  var fhunt=$('#rs500').val();
+                  var rs500=fhunt*500;
+                  var thous=$('#rs1000').val();
+                  var rs1000=thous*1000;
+                  var twthous=$('#rs2000').val();
+                  var rs2000=twthous*2000;
+                  var total=rs1+rs2+rs5+rs10+rs20+rs50+rs100+rs500+rs1000+rs2000;
+                  $('#total').val(total);
+                  var url="/currency/currency?operation=add&rs1="+rs1+"&rs2="+rs2+"&rs5="+rs5+"&rs10="+rs10+"&rs20="+rs20+"&rs50="+rs50+"&rs100="+rs100+"&rs500="+rs500+"&rs1000="+rs1000+"&rs2000="+rs2000+"&total="+total;
+                  $.ajax(url)
+                  .done(function(result){
+                   //console.log();
+                     //alert(result);
+                    alert("your's sucessfully added");
+                        }).
+                    fail(function(result){
+                    // console.log();
+                     //alert(result);
+                    })
+                    
       					});
       		});
 $(document).ready(function(){
@@ -35,4 +47,3 @@ $(document).ready(function(){
         $("#first").slideToggle("slow");
     });
 });
-          		
